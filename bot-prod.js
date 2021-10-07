252b426b361fa07aadae970545b9be32e01ce3f3
ninjadynamics <brunosilva.work@gmail.com>
2021-10-07 13:51:52 +0200

    Fixes

--- bot-prod.js
+++ bot-prod.js
@@ -1,4 +1,9 @@
+// Because I like Python
 const print = console.log;
+
+const TESTNET = true;
+const chain = TESTNET ? 'bombay-12' : 'columbus-5';
+const url = TESTNET ? 'https://bombay-lcd.terra.dev' : 'https://lcd.terra.dev';
 
 const DIVISOR = 1000000;
 const DENOM = {
@@ -6,12 +11,8 @@ const DENOM = {
     'UST': 'uusd'
 }
 
-//const chain = "bombay-12";
-const chain = "columbus-5";
-
-// connect to bombay testnet
 const terra = new Terra.LCDClient({
-    URL: 'https://lcd.terra.dev',
+    URL: url,
     chainID: chain,
 });
 
@@ -20,6 +21,7 @@ var mk = null;
 
 function loadWallet() {
     mk = new Terra.MnemonicKey({
+        // You have to set it in the console
         mnemonic: MNEMONIC
     });
     wallet = terra.wallet(mk);
@@ -49,41 +51,41 @@ function swap(amount, from, to) {
     })
     .then(tx => terra.tx.broadcast(tx))
     .then(result => {
-        print(`https://finder.terra.money/columbus-5/tx/${result.txhash}`);
+        print(`https://finder.terra.money/${chain}/tx/${result.txhash}`);
     });
 }
 
 function sellAt(amountLuna, target) {
     async function _sell(amount, target) {
         const p = await price();
-        print("Current LUNA price:", p);
+        print('Current LUNA price:', p);
         if (p >= target) {
-            print("Selling", amount, "LUNA at", p, "UST");
+            print('Selling', amount, 'LUNA at', p, 'UST');
             swap(amount, 'luna', 'ust')
             clearInterval(sellID);
         }
     }
-    print('Creating sell order of', amountLuna, "LUNA at", target, "UST");
+    print('Creating sell order of', amountLuna, 'LUNA at', target, 'UST');
     var sellID = setInterval(_sell, 2000, amountLuna, target);
 }
 
 function buyAt(amountUST, target) {
     async function _buy(amount, target) {
         const p = await price();
-        print("Current LUNA price:", p);
+        print('Current LUNA price:', p);
         if (p <= target) {
-            print("Buying", amount, "LUNA at", p, "UST");
+            print('Buying', amount / p, 'LUNA at', p, 'UST');
             swap(amount, 'ust', 'luna')
             clearInterval(buyID);
         }
     }
-    print('Creating buy order of', (amountUST / target), "LUNA at", target, "UST");
+    print('Creating buy order of', (amountUST / target), 'LUNA at', target, 'UST');
     var buyID = setInterval(_buy, 2000, amountUST, target);
 }
 
 async function start() {
     loadWallet();
     var b = await balance();
-    print('Balances:', b.uluna / DIVISOR, "LUNA /", b.uusd / DIVISOR, "UST");
-    print('1 LUNA =', await price(), "UST");
+    print('Balances:', b.uluna / DIVISOR, 'LUNA /', b.uusd / DIVISOR, 'UST');
+    print('1 LUNA =', await price(), 'UST');
 }
